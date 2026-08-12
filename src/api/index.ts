1bb433bde7d29181141a25b99ecaf7e82740cd55
Stephen Zhu <13322493517@163.com>
2026-08-12 12:07:35 +0800

    final

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,7 +12,7 @@ import type {
   GPSCheckinRequest,
 } from '../types';
 
-const API_BASE_URL = 'http://localhost:8080/api';
+const API_BASE_URL = '/backend-api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
